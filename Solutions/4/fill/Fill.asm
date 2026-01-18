394a93543f5d108b4235676a8fbbd4a67c26cc65
TheFlemoid <trey.dahlberg@gmail.com>
2026-01-18 15:20:31 -0600

    Did the Fill.asm project

--- Solutions/4/fill/Fill.asm
+++ Solutions/4/fill/Fill.asm
@@ -8,4 +8,60 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// Initialize variables
+@16384
+D=A
+@R3
+M=D
+
+// Check the keyboard register, if nothing is pressed it
+// will be 0x00, otherwise it will be something other then 0x00
+(LOOP)
+@24576
+D=M
+@WHITE
+D;JEQ
+@BLACK
+0;JMP
+
+// Set a VRAM word to white (0x00)
+(WHITE)
+D=0
+@R3
+A=M
+M=D
+@INC
+0;JMP
+
+// Set a VRAM word to black (0xFF)
+(BLACK)
+D=-1
+@R3
+A=M
+M=D
+@INC
+0;JMP
+
+// Increment R3, and check that we're still in VRAM
+(INC)
+@R3
+D=M+1
+M=D
+@24576
+D=A
+@R3
+D=D-M
+@RESET
+D;JEQ
+@LOOP
+0;JMP
+
+// Reset R3 back to the start of VRAM
+(RESET)
+@16384
+D=A
+@R3
+M=D
+@LOOP
+0;JMP
+
